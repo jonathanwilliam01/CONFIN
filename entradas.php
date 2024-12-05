c80bd82ca5aa9ab1f377d18ed83aa7124816a964
Jonathan William <jonathan.willian@embras.net>
2024-12-05 08:48:21 -0300

    V12.01

--- entradas.php
+++ entradas.php
@@ -30,21 +30,10 @@ $sql_entradas = $mysqli->query(query: $entradas) or die("Erro ao consultar" . $m
 </head>
 <body>
 <div class="container">
-<div class="lateral-menu">
-            <div class="perfil">
-                <div class="icone-perfil"><span class="material-icons" style="font-size: 40px;">account_circle</span></div>
-                <p class="nome-perfil"><a href="">Meu Perfil </a></p>
-            </div>
-
-            <div class="opcoes">
-                <a href="index.php"><div class="li"><div class="nome--menu">Dashboard</div><span class="material-icons" style="font-size: 2ch;">chevron_right</span></div></a>
-                <a href="saidas.php"><div class="li"><div class="nome--menu">Saídas</div><span class="material-icons" style="font-size: 2ch;">chevron_right</span></div></a>
-                <a href="entradas.php"><div class="li"><div class="nome--menu">Entradas</div><span class="material-icons" style="font-size: 2ch;">chevron_right</span></div></a>
-                <a href="metas.php"></a><div class="li"><div class="nome--menu">Metas</div><span class="material-icons" style="font-size: 2ch;">chevron_right</span></div></a>
-                <a href="relatorios.php"></a><div class="li"><div class="nome--menu">Relatórios</div><span class="material-icons" style="font-size: 2ch;">chevron_right</span></div></a>
-                <a href="configurações.php"></a><div class="li"><div class="nome--menu">Configurações</div><span class="material-icons" style="font-size: 2ch;">settings</span></div></a>
-            </div>
-        </div>
+<!-- MENU LATERAL -->
+    <?php
+        require_once('view-lateral-menu.php')
+        ?>
 
 <div class="conteudo">
 
